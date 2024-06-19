65ace875d28905936b43fa369546747fa30c0219
Oriol Bertomeu <oriol.bertomeuidiaz@dlr.de>
2024-06-19 22:38:38 +0200

    loader from api

--- band_generation/get_material_data.py
+++ band_generation/get_material_data.py
@@ -1,4 +1,4 @@
-from mp_api.client import MPRester
+#from mp_api.client import MPRester
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
@@ -8,20 +8,21 @@ import csv
 with open('material_data.csv','w') as write_file:
     data_writer = csv.writer(write_file)
     
-    with open('table_export.csv','r') as read_file:
-        data_reader = csv.reader(read_file)
-        
-        with MPRester(api_key="SFTntPIf0QnTef7f6sZKkBSwGILrxlXZ") as mpr:
-        bandstructure = mpr.get_bandstructure_by_material_id("mp-1095269")
+with open('table_export.csv','r') as read_file:
+    data_reader = csv.reader(read_file)
+    material_ids = [] 
+    for row in data_reader:
+        material_ids.append(row[2])
 
-        plt.figure()
-        for element in bandstructure.bands:
-            for i in range(bandstructure.nb_bands):
-                band = list(bandstructure.bands.values())[0][i] - bandstructure.efermi
-                krange = np.linspace(-np.pi, np.pi, len(band))
-                poly_coeffs = np.polyfit(krange, band, 20)
-                data_writer.writerow(poly_coeffs)
-                plt.plot(np.linspace(-np.pi, np.pi, len(band)), band)
+    material_ids.pop(0)
 
-plt.xlabel("k"); plt.ylabel("E"); plt.title("band structure")
-plt.show()
+    with MPRester(api_key="SFTntPIf0QnTef7f6sZKkBSwGILrxlXZ") as mpr:
+        for material in material_ids:
+            bandstructure = mpr.get_bandstructure_by_material_id(material)
+
+            for element in bandstructure.bands:
+                for i in range(bandstructure.nb_bands):
+                    band = list(bandstructure.bands.values())[0][i] - bandstructure.efermi
+                    krange = np.linspace(-np.pi, np.pi, len(band))
+                    poly_coeffs = np.polyfit(krange, band, 20)
+                    data_writer.writerow(poly_coeffs)
